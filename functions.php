718453c723addba1680dc0889c8cb23fa477ed89
ArrayIterator <arrayiterator@gmail.com>
2022-02-02 11:31:45 +0700

    patch

--- functions.php
+++ functions.php
@@ -58,6 +58,74 @@ if (!function_exists('news_tray_child_after_setup_theme')) {
 }
 add_action('after_setup_theme', 'news_tray_child_after_setup_theme');
 
-//add_action('admin_enqueue_scripts', function () {
-//	wp_enqueue_style('news-tray-child-custom', get_theme_file_uri('assets/css/custom.css'));
-//});
+/*
+add_action('init', function () {
+	global $wp;
+	$wp->add_query_var('s_map');
+	add_rewrite_rule( '^s_map\.xml$', 'index.php?s_map=1', 'top' );
+});
+
+add_action('template_redirect', function () {
+	$map = get_query_var('s_map');
+	if (!$map) {
+		return;
+	}
+	header("Cache-Control: no-store, no-cache, must-revalidate", true, 200);
+	header('Content-Type: text/xml; charset=UTF-8', true, 200);
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	echo '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" '
+	. 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd '
+	. 'http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" '
+	. 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n";
+	$args = array(
+		'post_status' => 'publish',
+		'order' => 'DESC',
+		'orderby' => 'DATE',
+		'post_type' => 'post',
+		'ignore_sticky_posts' => 1,
+		'date_query' => [
+			[
+//				'year'  => $today['year'],
+//				'month' => $today['mon'],
+//				'day'   => $today['mday'],
+			],
+		],
+		'per_page' => 500,
+	);
+	$query = new WP_Query($args);
+	$c = 0;
+	while ($query->have_posts()) {
+		$query->the_post();
+		$post = $query->post;
+		$date = get_gmt_from_date($post->post_date, 'c');
+		if ($c++ === 0) {
+			echo "<url>\n";
+			echo "<loc>" . esc_url(home_url()) . "</loc>\n";
+			echo "<lastmod>{$date}</lastmod>\n";
+			echo "</url>\n";
+		}
+		echo "<url>\n";
+		echo "<loc>" . esc_url(get_permalink($post)) . "</loc>\n";
+		echo "<lastmod>{$date}</lastmod>\n";
+		$image = get_attached_media('image', $post);
+		if (!empty($image)) {
+			foreach ($image as $img) {
+				$excerpt = (wp_get_attachment_caption($img->ID));
+				$excerpt = $excerpt && is_string($excerpt) ? esc_xml($excerpt) : '';
+				$title = esc_xml(get_the_title($img));
+				$permalink = esc_url(get_permalink($img));
+				echo "<image:image>\n";
+				echo "<image:loc>{$permalink}</image:loc>\n";
+				echo "<image:title><![CDATA[{$title}]]></image:title>\n";
+				echo "<image:caption><![CDATA[{$excerpt}]]></image:caption>\n";
+				echo "</image:image>\n";
+			}
+
+		}
+		echo "</url>\n";
+	}
+	echo "</urlset>\n";
+	unset($query);
+	exit;
+}, 9);
+*/
